59505221eac205d6459df7c224f1ff286bc3b5b4
samuel bonu <samuelbonux10@gmail.com>
2025-06-25 16:36:18 +0100

    max profit problem

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main(){
-	my_slice := []int{9, 2, 5, 3, 6, 4, 8, 1, 7}
+	prices := []int{7, 1, 5, 3, 6, 4}
+
+	brute_max_profit := brute_max_profit_day(prices)
+	fmt.Println(brute_max_profit)
+
+	max_profit := max_profit_day(prices)
+	fmt.Println(max_profit)
 }
--- max_profit.go
+++ max_profit.go
@@ -1,6 +1,29 @@
 package main
 
-func max_profit(my_slice []int) int {
+func brute_max_profit_day(prices []int) int {
 	//brute force
-	
+	max_profit := 0
+	for i:=0; i<len(prices); i++{
+		for j:=i+1; j<len(prices); j++ {
+			diff := prices[j] - prices[i]
+			if diff > max_profit{
+				max_profit = diff
+			}
+		}
+	}
+	return max_profit
+}
+
+func max_profit_day(prices []int) int {
+	//brute force
+	max_profit := 0
+	min_profit := prices[0]
+	for i:=0; i<len(prices); i++ {
+		if prices[i] < min_profit{
+			min_profit = prices[i]
+		} else if prices[i] - min_profit > max_profit{
+			max_profit = prices[i] - min_profit
+		}
+	}
+	return max_profit
 }
