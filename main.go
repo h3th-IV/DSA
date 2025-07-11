59adef4d92a18958ac25c43beed9b6f867f24524
samuel bonu <samuelbonux10@gmail.com>
2025-07-11 15:41:34 +0100

    thrteader

--- main.go
+++ main.go
@@ -4,31 +4,51 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	items = append(items, Item{Id: 1, Name: "ProArt16", Quantity: 10})
-	items = append(items, Item{Id: 1, Name: "ProArt17", Quantity: 15})
+	mult := mux.NewRouter()
+	mult.HandleFunc("/get-item", GetAnmItem).Methods(http.MethodGet)
+	mult.HandleFunc("/new-item", addNewItem).Methods(http.MethodPost)
+	mult.HandleFunc("/health", CheckServerHealth).Methods((http.MethodGet))
 
-	fmt.Println("test server running at :7005")
-	http.HandleFunc("/get-all", getAllItem)
-	http.ListenAndServe(":7005", nil)
+	fmt.Println("server running at port 9000")
+	http.ListenAndServe(":9000", mult)
+
 }
 
 type Item struct {
-	Id       int
-	Name     string
-	Quantity int
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Size string `json:"size"`
 }
 
-var items []Item
+func addNewItem(w http.ResponseWriter, r *http.Request) {
+	var item Item
 
-func getAllItem(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w).Encode("Method not allowed")
+	w.Header().Add("Content-type", "application/json")
+	err := json.NewDecoder(r.Body).Decode(item)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("unable to process request body")
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(items)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Add("Content-type", "application/json")
+	json.NewEncoder(w).Encode(item)
+}
+
+func CheckServerHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-type", "application/json")
+	json.NewEncoder(w).Encode("Server Connected Successfully")
+}
+
+func GetAnmItem(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-type", "application/json")
+	json.NewEncoder(w).Encode("Item fetched successfully")
 }
